57dd85b7c37cf8d8d871ce55d6916569436d2cb3
Unknown <g1727041@st.t-kougei.ac.jp>
2018-06-04 18:35:25 +0900

    最後わからなかった

---
+++ CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDirector : MonoBehaviour {
+    GameObject hpGauge;
+
+	// Use this for initialization
+	void Start () {
+        this.hpGauge = GameObject.Find("hpGauge");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.hpGauge.GetComponent<Image>().fillAmount -= 0.1f;
+		
+	}
+}
